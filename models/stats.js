51f1ec7fce316e2ae19491bd12a464bffe6e776f
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-08-01 23:21:16 -0400

    progress on models

---
+++ models/stats.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Stats extends Model {}
+
+Stats.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {  
+    bestRun: {
+      type: DataTypes.TIME,
+      }
+  },
+  {
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'stats',
+  }
+);
+
+module.exports = Stats;
