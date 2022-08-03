42f42689ebca314f51cc36f9ae7fa6829194459d
canslercp <canslercp@gmail.com>
2022-08-03 14:45:08 -0400

    idk

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const loginRoutes = require('./api/loginRoutes')
 
+router.use('/', loginRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
