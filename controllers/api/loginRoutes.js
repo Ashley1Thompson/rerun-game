2b1300d5372044b7f824c911550025171225f5bf
canslercp <canslercp@gmail.com>
2022-08-04 17:09:18 -0400

    completed logout route and js file

--- controllers/api/loginRoutes.js
+++ controllers/api/loginRoutes.js
@@ -90,6 +90,14 @@ router.post('/', async (req, res) => {
 });
 
 // Logout
-
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
