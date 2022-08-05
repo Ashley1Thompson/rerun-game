5a32d0662047a89ed5115ab93f311fed572ffb50
Auggie <miaugus@gmail.com>
2022-08-04 21:48:30 -0400

    all sounds fully functional

--- public/js/game.js
+++ public/js/game.js
@@ -1,42 +1,17 @@
-// let timer;
-// const startTime = 5; // in minutes
-// let time = startTime * 60; // in seconds
-// let formattedTime = time / 60;
-
-// // start button on-click:
-// function start() {
-//   timer = setInterval(updateTimer, 1000);
-//   updateTimer();
-//   // hide restart button while timer is running
-//   $("#playGameBtn").hide();
-// }
-// export { start };
-// // stop the clock
-// function stop() {
-//   clearInterval(timer);
-//   // return time left using time, helper fn to convert seconds into minutes and seconds.
-//   return formattedTime;
-// }
-// export { stop };
-
-// // The game ends when timer runs out, show restart button
-// function timesUp() {
-//   stop();
-
-//   $("#playGameBtn").show();
-// }
-
-// // handles countdown, calls timesUp to end game when no time left.
-// function updateTimer() {
-//   const minutes = Math.floor(time / 60);
-//   const seconds = time % 60;
-//   time--;
-//   if (time >= 0)
-//     document.getElementById("timer").innerHTML = `${minutes}:${seconds}`;
-//   else {
-//     timesUp();
-//   }
-// }
+// SOUNDPLAY
+const airS = new Audio("sounds/air.mp3");
+const airLockS = new Audio("sounds/airLock.mp3");
+const bigDoorS = new Audio("sounds/bigDoor.mp3");
+const droningThrumS = new Audio("sounds/droningThrum.mp3");
+const elecGlitchS = new Audio("sounds/elecGlitch.mp3");
+const elecThrumS = new Audio("sound/elecThrum.mp3");
+const failGlitchS = new Audio("sounds/failGlitch.mp3");
+const fanS = new Audio("sounds/fan.mp3");
+const kitchenS = new Audio("sounds/kitchen.mp3");
+const laboratoryS = new Audio("sounds/laboratory.mp3");
+const quickBeepS = new Audio("sounds/quickBeep.mp3");
+const runningWaterS = new Audio("sounds/runningWater.mp3");
+const smallDoorS = new Audio("sounds/smallDoor.mp3");
 
 // const sound = require("sound-play");
 const beginEl = document.querySelector(".begin");
@@ -119,9 +94,89 @@ atriumLsix.addEventListener("click", engineering6);
 engineeringLsix.addEventListener("click", science6Fail);
 engineeringRsix.addEventListener("click", science6Success);
 
-function testSound() {
-  sound.play("sounds/testSound.mp3");
-  console.log("done");
+// SOUND FUNCTIONS
+function airOn() {
+  airS.play();
+}
+
+function airOff() {
+  airS.pause();
+}
+
+function airLock() {
+  airLockS.play();
+}
+
+function bigDoorOn() {
+  bigDoorS.play();
+}
+
+function bigDoorOff() {
+  bigDoorS.pause();
+}
+
+function droningThrumOn() {
+  droningThrumS.play();
+}
+
+function droningThrumOff() {
+  droningThrumS.pause();
+}
+
+function elecGlitch() {
+  elecGlitchS.play();
+}
+
+function elecThrumOn() {
+  elecThrumS.play();
+}
+
+function elecThrumOff() {
+  elecThrumS.pause();
+}
+
+function failGlitch() {
+  failGlitchS.play();
+}
+
+function fanOn() {
+  fanS.play();
+}
+
+function fanOff() {
+  fanS.pause();
+}
+
+function kitchenOn() {
+  kitchenS.play();
+}
+
+function kitchenOff() {
+  kitchenS.pause();
+}
+
+function laboratoryOn() {
+  laboratoryS.play();
+}
+
+function laboratoryOff() {
+  laboratoryS.pause();
+}
+
+function quickBeep() {
+  quickBeepS.play();
+}
+
+function runningWaterOn() {
+  runningWaterS.play();
+}
+
+function runningWaterOff() {
+  runningWaterS.pause();
+}
+
+function smallDoor() {
+  smallDoorS.play();
 }
 
 // Functions to move between "rooms"
@@ -130,13 +185,17 @@ function beginGame() {
   atrium1El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
   // START TIMER HERE
-  start();
+  smallDoor();
+  droningThrumOn();
 }
 
 function engineering1() {
   atrium1El.classList.add("hide");
   engineering1El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/corridor-2.png";
+  droningThrumOff();
+  failGlitch();
+  fanOn();
 }
 
 function atrium1() {
@@ -144,6 +203,9 @@ function atrium1() {
   kitchen1El.classList.add("hide");
   atrium1El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  fanOff();
+  droningThrumOn();
+  kitchenOff();
 }
 function kitchen1() {
   atrium1El.classList.add("hide");
@@ -151,6 +213,8 @@ function kitchen1() {
   command2El.classList.add("hide");
   kitchen1El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/kitchen-new.png";
+  droningThrumOff();
+  kitchenOn();
 }
 
 function acknowledge() {
@@ -159,12 +223,15 @@ function acknowledge() {
   commandHiddenTwoEl.classList.remove("hide");
   document.getElementById("acknowledgementEl").innerText =
     '"Thank you for acknowledging the contamination alert. Don\'t forget to like and subscribe for more contamination alert content."';
+  quickBeep();
 }
 
 function command1() {
   kitchen1El.classList.add("hide");
   command1El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/command-room.png";
+  kitchenOff();
+  airOn();
 }
 
 function botany2() {
@@ -172,6 +239,8 @@ function botany2() {
   command2El.classList.add("hide");
   botany2El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/botany-room.png";
+  airOff();
+  runningWaterOn();
 }
 
 function command2() {
@@ -179,6 +248,9 @@ function command2() {
   kitchen2El.classList.add("hide");
   command2El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/command-room.png";
+  runningWaterOff();
+  kitchenOff();
+  airOn();
 }
 
 function kitchen2() {
@@ -187,29 +259,40 @@ function kitchen2() {
   atrium2El.classList.add("hide");
   kitchen2El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/kitchen-new.png";
+  airOff();
+  droningThrumOff();
+  kitchenOn();
 }
 
 function atrium2() {
   kitchen2El.classList.add("hide");
   atrium2El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  kitchenOff();
+  droningThrumOn();
 }
 
 function engineering2() {
   atrium2El.classList.add("hide");
   engineering2El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/airlock-door.png";
+  droningThrumOff();
+  quickBeep();
+  fanOn();
 }
 
 function atrium3() {
   engineering2El.classList.add("hide");
   atrium3El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  fanOff();
+  droningThrumOn();
 }
 
 function atriumFail() {
   document.getElementById("atriumSearch").innerText =
     "No badge here ... but at least you know where it isn't! Unfortunately, you have lost some precious seconds.";
+  failGlitch();
   // DECREMENT TIMER BY 30 SECONDS
 }
 
@@ -217,11 +300,14 @@ function kitchen3() {
   atrium3El.classList.add("hide");
   kitchen3El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/kitchen-new.png";
+  droningThrumOff();
+  kitchenOn();
 }
 
 function kitchenFail() {
   document.getElementById("kitchenSearch").innerText =
     "No badge here ... but you notice that you ate your leftover sushi from Wednesday while sleep-walking. Bummer, you were hoping to have that for dinner. You have lost some precious seconds.";
+  failGlitch();
   // DECREMENT TIMER BY 30 SECONDS
 }
 
@@ -229,11 +315,14 @@ function command3() {
   kitchen3El.classList.add("hide");
   command3El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/command-room.png";
+  kitchenOff();
+  airOn();
 }
 
 function commandFail() {
   document.getElementById("commandSearch").innerText =
     "You had hoped that the glow from the terminals might help you catch a glint off the reflective sheen on your ID badge, but no such luck, looks like it isn't here. You have lost some precious seconds.";
+  failGlitch();
   // DECREMENT TIMER BY 30 SECONDS
 }
 
@@ -241,6 +330,8 @@ function botany3() {
   command3El.classList.add("hide");
   botany3El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/botany-room.png";
+  airOff();
+  runningWaterOn();
 }
 
 function botanySuccess() {
@@ -248,41 +339,53 @@ function botanySuccess() {
     "As soon as you look down and see your ID badge on the floor, you get a memory of the last thing that happened before you woke up in your living quarters: you had taken a nice deep breath near the flowers … that must have been when the odorless fumes from the compromised test vial proved to be too strong. ID badge recovered!";
   botanyHiddenOneEl.classList.add("hide");
   botanyHiddenTwoEl.classList.remove("hide");
+  quickBeep();
 }
 
 function command4() {
   botany3El.classList.add("hide");
   command4El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/command-room.png";
+  runningWaterOff();
+  airOn();
 }
 
 function kitchen4() {
   command4El.classList.add("hide");
   kitchen4El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/kitchen-new.png";
+  airOff();
+  kitchenOn();
 }
 
 function atrium4() {
   kitchen4El.classList.add("hide");
   atrium4El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  kitchenOff();
+  droningThrumOn();
 }
 
 function engineering4() {
   atrium4El.classList.add("hide");
   engineering4El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/engineering-new.png";
+  droningThrumOff();
+  fanOn();
 }
 
 function science4() {
   engineering4El.classList.add("hide");
   science4El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/science-bay-light.png";
+  fanOff();
+  laboratoryOn();
 }
 
 function engineeringFailTwo() {
   document.getElementById("engineeringSearchTwo").innerText =
     "The only thing sticky in here is the knowledge that Dr. Jung has bestowed upon you ... the clock ticks down ...";
+  failGlitch();
   // DECREMENT TIMER BY 30 SECONDS
 }
 
@@ -290,17 +393,22 @@ function engineering5() {
   science4El.classList.add("hide");
   engineering5El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/engineering-new.png";
+  laboratoryOff();
+  fanOn();
 }
 
 function atrium5() {
   engineering5El.classList.add("hide");
   atrium5El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  fanOff();
+  droningThrumOn();
 }
 
 function atriumFailTwo() {
   document.getElementById("atriumSearchTwo").innerText =
     "I mean, you can't really be that surprised, right? No dice. The clock ticks down ...";
+  failGlitch();
   // DECREMENT TIMER BY 30 SECONDS
 }
 
@@ -308,6 +416,8 @@ function kitchen5() {
   atrium5El.classList.add("hide");
   kitchen5El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/kitchen-new.png";
+  droningThrumOff();
+  kitchenOn();
 }
 
 function kitchenSuccess() {
@@ -316,12 +426,14 @@ function kitchenSuccess() {
   kitchenHiddenOneEl.classList.add("hide");
   kitchenHiddenTwoEl.classList.remove("hide");
   kitchenHiddenThreeEl.classList.remove("hide");
+  quickBeep();
 }
 
 function kitchenSnackSuccess() {
   document.getElementById("kitchenSearchTwo").innerText =
     "You hurriedly chug some banana milk ... and suddenly you feel just a bit better! That little pick me up has helped you move faster!";
   kitchenHiddenThreeEl.classList.add("hide");
+  quickBeep();
   // ADD 15 SECONDS TO CLOCK
 }
 
@@ -329,22 +441,29 @@ function atrium6() {
   kitchen5El.classList.add("hide");
   atrium6El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/atrium-1.png";
+  kitchenOff();
+  droningThrumOn();
 }
 
 function engineering6() {
   atrium6El.classList.add("hide");
   engineering6El.classList.remove("hide");
   document.getElementById("spaceShipImg").src = "images/engineering-new.png";
+  droningThrumOff();
+  fanOn();
 }
 
 function science6Fail() {
   document.getElementById("engineeringDoors").innerText =
     "Where you could have sworn you just saw a door, there is now only wall—and you walk right into it. You wasted a few precious seconds, but the impact helped keep you awake, so I guess it all worked out ...";
   engineeringWallEl.classList.add("hide");
+  failGlitch();
 }
 
 function science6Success() {
   engineering6El.classList.add("hide");
   finaleEl.classList.remove("hide");
+  quickBeep();
+  fanOff();
   // STOP TIMER
 }
